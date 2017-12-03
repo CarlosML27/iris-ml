6b11c75a01bdc3f1413b2832b53b75860e6cb39a
Carlos Morente <carlosmorentedev@gmail.com>
2017-12-03 18:57:15 +0100

    Added training and testing split for data

--- src/iris-ml.py
+++ src/iris-ml.py
@@ -1,19 +1,45 @@
 import csv
 import argparse
+import random
 
-defaultfilename = '../data/iris.csv'
+DEFAULT_FILENAME = '../data/iris.csv'
+SPLIT_RATIO = 0.66
 
-parser = argparse.ArgumentParser()
-parser.add_argument("--original", help="use R.Fisher original data", action='store_true')
-args = parser.parse_args()
-filename = defaultfilename
-if args.original:
-	filename = '../data/iris-original.csv'
-with open(filename, 'rt') as csvfile:
-	dataset = csv.reader(csvfile)
-	datasetlist = list(dataset)
-	rows = datasetlist[1:]
-	separator = ', '	
-	print(filename)
+def retrieve_args():
+	parser = argparse.ArgumentParser()
+	parser.add_argument("-o", "--original", help="use R.Fisher original data", action='store_true')
+	return parser.parse_args()
+	
+def get_filename(args):
+	return '../data/iris-original.csv' if args.original else DEFAULT_FILENAME
+
+def print_rows(rows):
+	separator = ', '
 	for row in rows:
 		print(separator.join(row))
+
+def get_rows(filename):
+	with open(filename, 'rt') as csvfile:	
+		dataset = csv.reader(csvfile)
+		datasetlist = list(dataset)
+		return datasetlist[1:]
+
+def split_data(data, split_ratio):
+	training_set = []
+	test_set = []
+	for x in range(len(data)):
+		for y in range(4):
+			data[x][y] = float(data[x][y])
+		if random.random() < split_ratio:
+			training_set.append(data[x])
+		else:
+			test_set.append(data[x])	
+	return training_set, test_set
+
+
+args = retrieve_args()
+filename = get_filename(args)
+rows = get_rows(filename)
+training_set, test_set = split_data(rows, SPLIT_RATIO)
+print("Train: {}".format(repr(len(training_set))))
+print("Test: {}".format(repr(len(test_set))))
