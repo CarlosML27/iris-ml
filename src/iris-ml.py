8c796d1d29dc7b23a71e01fd1556089dbeca97b2
Carlos Morente <carlosmorentedev@gmail.com>
2017-12-03 18:21:39 +0100

    Added argument option to use original data

--- src/iris-ml.py
+++ src/iris-ml.py
@@ -1,11 +1,19 @@
 import csv
+import argparse
 
-filename = '../data/iris.csv'
+defaultfilename = '../data/iris.csv'
 
+parser = argparse.ArgumentParser()
+parser.add_argument("--original", help="use R.Fisher original data", action='store_true')
+args = parser.parse_args()
+filename = defaultfilename
+if args.original:
+	filename = '../data/iris-original.csv'
 with open(filename, 'rt') as csvfile:
 	dataset = csv.reader(csvfile)
 	datasetlist = list(dataset)
 	rows = datasetlist[1:]
-	separator = ', '
+	separator = ', '	
+	print(filename)
 	for row in rows:
 		print(separator.join(row))
